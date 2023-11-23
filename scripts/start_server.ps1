9d2ddbab13f8c48a75c14e2c5c5492decdcd20e0
youngminkk <146568255+youngminkk@users.noreply.github.com>
2023-11-23 23:16:08 +0900

    Update start_server.ps1

--- scripts/start_server.ps1
+++ scripts/start_server.ps1
@@ -1,4 +1,4 @@
 $JAR_NAME = "demo-0.0.1-SNAPSHOT.jar"
 $REPOSITORY = "C:\spring"
 
-Start-Process -NoNewWindow -FilePath "java" -ArgumentList "-jar", "$REPOSITORY\$JAR_NAME"
+Start-Process -NoNewWindow -FilePath "java" -ArgumentList "-jar", "$REPOSITORY\$JAR_NAME" -RedirectStandardOutput "log.txt" -RedirectStandardError "error.txt"
