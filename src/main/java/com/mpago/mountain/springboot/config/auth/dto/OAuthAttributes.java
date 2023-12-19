60e89d8981e014ed49497e700768634d8197c70b
youngminkk <mmmin0610@gmail.com>
2023-12-19 09:55:48 +0900

    ignore 수정

---
+++ src/main/java/com/mpago/mountain/springboot/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,78 @@
+package com.mpago.mountain.springboot.config.auth.dto;
+
+import com.mpago.mountain.springboot.domain.user.Role;
+import com.mpago.mountain.springboot.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        System.out.println("registrationId: " + registrationId);
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+        else if("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+        System.out.println("Kakao Account: " + kakaoAccount);
+        System.out.println("Profile: " + profile);
+        return OAuthAttributes.builder()
+                .name(String.valueOf(profile.get("nickname")))
+                .email(String.valueOf(kakaoAccount.get("email")))
+                .picture(String.valueOf(profile.get("profile_image_url")))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
---
+++ src/test/java/com/mpago/mountain/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpago.mountain.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
